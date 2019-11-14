511607c3b4a278db1bac45d0683771f15192162d
unluerbaris <unluerbaris@gmail.com>
2019-11-14 16:19:35 +0900

    Horizontal movement

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WOS.Movement;
+
+namespace WOS.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        Mover mover;
+
+        private void Start()
+        {
+            mover = GetComponent<Mover>();
+        }
+
+        void Update()
+        {
+            float controlThrow = Input.GetAxis("Horizontal"); //-1 to +1
+            mover.Walk(controlThrow);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WOS.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] float walkSpeed = 6f;
+
+        Rigidbody2D rb2D;
+
+        private void Start()
+        {
+            rb2D = GetComponent<Rigidbody2D>();
+        }
+
+        public void Walk(float controlThrow)
+        {
+            Vector2 characterVelocity = new Vector2(controlThrow * walkSpeed, rb2D.velocity.y);
+            rb2D.velocity = characterVelocity;
+        }
+    }
+}
+
