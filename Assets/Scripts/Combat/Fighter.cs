aaa5df7eeab6feff138362fb6f799c5fbe35a87a
unluerbaris <unluerbaris@gmail.com>
2019-11-15 22:37:46 +0900

    Attack animation

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WOS.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        Animator animator;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void PlayAttackAnimation()
+        {
+            animator.SetTrigger("isAttacking");
+        }
+    }
+}
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,27 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using WOS.Movement;
+using WOS.Combat;
 
 namespace WOS.Control
 {
     public class PlayerController : MonoBehaviour
     {
         Mover mover;
+        Fighter fighter;
 
         private void Start()
         {
             mover = GetComponent<Mover>();
+            fighter = GetComponent<Fighter>();
         }
 
         void Update()
         {
             PlayerRunUpdate();
+            AttackUpdate();
         }
 
         private void PlayerRunUpdate()
         {
             float controlThrow = Input.GetAxis("Horizontal"); //-1 to +1
             mover.Walk(controlThrow);
+        }
+
+        private void AttackUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                fighter.PlayAttackAnimation();
+            }
         }
     }
 }
