9aa7bc5477ca41c24d0f8b5a608b7371963907a6
unluerbaris <unluerbaris@gmail.com>
2019-12-02 10:25:59 +0900

    Enemy spawner

---
+++ Assets/Scripts/Core/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WOS.Core
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] GameObject enemy;
+        bool enemySpawned = false;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!enemySpawned && collision.gameObject.tag == "Respawn")
+            {
+                enemySpawned = true;
+                GameObject enemyClone = Instantiate(enemy, transform.position, Quaternion.identity) as GameObject;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
