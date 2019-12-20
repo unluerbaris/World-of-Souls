41a479c83973eff61395f103a0694ab71802f847
unluerbaris <unluerbaris@gmail.com>
2019-12-20 11:01:51 +0900

    Scene loader

--- Assets/Scripts/Control/EnemyAI.cs
+++ Assets/Scripts/Control/EnemyAI.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using WOS.Movement;
 
 namespace WOS.Control
---
+++ Assets/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace WOS.Core
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        int currentSceneIndex;
+
+        public void LoadNextScene()
+        {
+            currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+            {
+                SceneManager.LoadScene(currentSceneIndex + 1);
+            }
+        }
+
+        public void LoadCurrentScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public void LoadMainMenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
