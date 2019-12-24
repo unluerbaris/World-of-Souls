e5cb12bc253b5619bb3e421c40a73b303ed06e86
unluerbaris <unluerbaris@gmail.com>
2019-12-24 22:05:45 +0900

    Button hover and click SFX

---
+++ Assets/Scripts/Core/ButtonSFX.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace WOS.Core
+{
+    public class ButtonSFX : MonoBehaviour
+    {
+        AudioManager audioManager;
+
+        void Start()
+        {
+            audioManager = FindObjectOfType<AudioManager>();
+        }
+
+        public void PlayHoverSFX()
+        {
+            audioManager.PlaySound("Hover");
+        }
+
+        public void PlayClickSFX()
+        {
+            audioManager.PlaySound("Click");
+        }
+    }
+}
